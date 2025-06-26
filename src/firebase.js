253547395791e8e0de32291403d8307e3c788027
kotani6053 <yoshioka@e-kotani.co.jp>
2025-06-26 11:28:23 +0900

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtLFNwnAqBYFR7eFi-bKaT4Vq9Zu15a6I",
+  authDomain: "kotaniapp-4f017.firebaseapp.com",
+  projectId: "kotaniapp-4f017",
+  storageBucket: "kotaniapp-4f017.firebasestorage.app",
+  messagingSenderId: "623409374889",
+  appId: "1:623409374889:web:8aba02fed565f56c23abb8"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db };
