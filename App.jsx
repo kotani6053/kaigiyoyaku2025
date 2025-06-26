c7d2874ca01aa0e44309d69e3a16571dd357be5a
kotani6053 <yoshioka@e-kotani.co.jp>
2025-06-26 09:26:17 +0900

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,4 @@
 import React from 'react'; export default function App() { return <h1>KOTANI会議室予約アプリ</h1>; }
+import { db } from "./firebase";
+import { collection, addDoc, getDocs, deleteDoc, doc, onSnapshot } from "firebase/firestore";
+import { useEffect } from "react";
