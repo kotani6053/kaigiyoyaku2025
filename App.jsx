918f74771fe7a27d172a10f827f1e275a5cacbcc
kotani6053 <yoshioka@e-kotani.co.jp>
2025-07-02 15:02:05 +0900

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase";
@@ -10,15 +11,21 @@ const App = () => {
 
   const fetchReservations = async () => {
     const snapshot = await getDocs(collection(db, "reservations"));
-    setReservations(snapshot.docs.map((doc) => doc.data()));
+    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    setReservations(data);
   };
 
   useEffect(() => {
     fetchReservations();
   }, []);
 
+  useEffect(() => {
+    console.log("全予約データ", reservations);
+  }, [reservations]);
+
   return (
-    <div className="max-w-2xl mx-auto mt-4">
+    <div className="max-w-2xl mx-auto mt-4 p-4">
+      {/* 必要なら日付変更UIをここに入れる */}
       <ReservationForm selectedDate={selectedDate} onReserved={fetchReservations} />
       <CalendarView reservations={reservations} selectedDate={selectedDate} />
     </div>
